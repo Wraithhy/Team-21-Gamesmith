dd0fbbf7b94c37405badd1e7f946af4bd8b650bb
Khalil Rizwan <220231190@aston.ac.uk>
2023-11-27 15:47:57 +0000

    Rajmeer : Navbar change and CSS for Home Page

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -6,36 +6,36 @@
     <title>GameSmith</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <style>
-body {
-  font-family: "Roboto", sans-serif;
-}
+
 </style>
+<link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
 </head>
 <body>
+@include('navbar')
 
 <div class="container" >
     <div class="text-center">
-        <h1> Welcome To Gamesmith</h1>
+        <h1 class="mh"> Welcome To Gamesmith</h>
     </div>
 </div>
 
 <div class="container">
     <div class="text-center">
-        <p> Welcome to Gamesmith, where gaming prowess meets unparalleled gear. Elevate your play, conquer with us!</p>
+        <p class="text"> Welcome to Gamesmith, where gaming prowess meets unparalleled gear. Elevate your play, conquer with us!</p>
 
     </div>
 </div>
 
 <div class="container">
     <div class="text-center">
-        <h2>Our Product Line!</h2>
-        <p> Discover elite gaming gear in our curated categories!</p>
-        <img src="/images/m1.png" class="rounded" alt="..." height="100">
-        <img src="/images/m1.png" class="rounded" alt="..." height="100">
-        <img src="/images/m1.png" class="rounded" alt="..." height="100">
-        <img src="/images/m1.png" class="rounded" alt="..." height="100">
-        <img src="/images/m1.png" class="rounded" alt="..." height="100">
+        <h2 class="text">Our Product Line!</h2>
+        <p class="text"> Discover elite gaming gear in our curated categories!</p>
+        <img class="catimg" src="/images/m1.png" class="rounded" alt="..." height="100">
+        <img class="catimg"src="/images/m1.png" class="rounded" alt="..." height="100">
+        <img class="catimg"src="/images/m1.png" class="rounded" alt="..." height="100">
+        <img class="catimg"src="/images/m1.png" class="rounded" alt="..." height="100">
+        <img class="catimg"src="/images/m1.png" class="rounded" alt="..." height="100">
 
     </div>
 </div>
@@ -43,10 +43,12 @@ body {
 <dic class="container">
     <div class="text-center">
         <h2>Featured Gems</h2>
-        <p>Unleash Gaming Excellence</p>
+        <p class="text">Unleash Gaming Excellence</p>
     </div>
 </dic>
 
+
+<div class="container">
 <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
   <div class="carousel-indicators">
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
@@ -55,24 +57,24 @@ body {
   </div>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="/images/m1.png" class="d-block w-100" alt="..."  >
+      <img src="/images/m1.png" class="d-block w-100" alt="..."  height="600" >
       <div class="carousel-caption d-none d-md-block">
         <h5>First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
+        <p class="text">Some representative placeholder content for the first slide.</p>
       </div>
     </div>
     <div class="carousel-item">
-      <img src="/images/m1.png" class="d-block w-100" alt="...">
+      <img src="/images/m1.png" class="d-block w-100" alt="..." height="600">
       <div class="carousel-caption d-none d-md-block">
         <h5>Second slide label</h5>
-        <p>Some representative placeholder content for the second slide.</p>
+        <p class="text">Some representative placeholder content for the second slide.</p>
       </div>
     </div>
     <div class="carousel-item">
-      <img src="/images/m1.png" class="d-block w-100" alt="...">
+      <img src="/images/m1.png" class="d-block w-100" alt="..." height="600">
       <div class="carousel-caption d-none d-md-block">
         <h5>Third slide label</h5>
-        <p>Some representative placeholder content for the third slide.</p>
+        <p class="text">Some representative placeholder content for the third slide.</p>
       </div>
     </div>
   </div>
@@ -85,12 +87,7 @@ body {
     <span class="visually-hidden">Next</span>
   </button>
 </div>
-
-
-
-
-
-
+</div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
   </body>
