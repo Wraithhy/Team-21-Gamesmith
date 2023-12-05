6e22e918473a0a465da0da0e4b07cb97503742a8
OwenR1 <220131694@aston.ac.uk>
2023-12-05 14:41:49 +0000

    register-v2
    Added styling to register.
    Added navbar to register.
    Removed layouts page used for testing.
    Added layouts code to register file.
    Moved CSS to its own file.
    Register page state: working.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,12 +1,21 @@
-@extends('auth.layouts')
+@include('navbar')
 
-@section('content')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>GameSmith Register</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
 
+<link rel="stylesheet" href= "{{asset('css/style.css')}}" />
 <div class="row justify-content-center mt-5">
     <div class="col-md-8">
 
         <div class="card">
-            <div class="card-header">Register</div>
+            <div class="card-header row justify-content-center">GameSmith Register</div>
             <div class="card-body">
                 <form action="{{ route('store') }}" method="post">
                     @csrf
@@ -53,4 +62,3 @@
     </div>    
 </div>
     
-@endsection
