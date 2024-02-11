d3050dfc5f67f95821191430254b75d112454389
AlexWR1 <220185217@Aston.ac.uk>
2024-02-11 21:43:13 +0000

    Big CSS update for Login
    Changed colour scheme to match overall website design, changed button functionality and added Register button.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,6 +8,10 @@
                 <form action="{{ route('authenticate') }}" method="post">
                     @csrf
                     <div class="mb-3 row">
+
+                    <a href="/" class="LoginIMG">
+                <img class="LoginIMG" src="images/gamesmith.png" alt="GameSmith Logo" />
+            </a>
                         <label for="email" class="col-md-4 col-form-label text-md-end text-start">Email Address</label>
                         <div class="col-md-6">
                           <input type="email" class="form-control @error('email') is-invalid @enderror" id="email" name="email" value="{{ old('email') }}">
@@ -25,29 +29,28 @@
                             @endif
                         </div>
                     </div>
-                    <div class="mb-3 row">
-                        <input type="submit" class="col-md-3 offset-md-5 btn btn-primary" value="Login">
-                    </div>
-                    
+                    <div class="logbutton-container">
+                        <input type="submit" class="logbtn" value="Login">
+                       
                 </form>
+
+                <a href= "register"> <button class="regbtn">Register</button> </a>
+</div>
             </div>
                 
             </div>
             
             <div class="contact-right">
-                <h3>Reach us </h3>
+                <h3>Having issues?</h3>
+                <h4>Get in Touch: </h4>
 
                 <table>
                     <tr>
-                        <td>GameSmith</td>
-                        <td></td>
-
+                        <td>&#128383 Phone:</td>
+                        <td>0800 03 1337</td>
                     </tr>
                     <tr>
-                        <td>Group 21</td>
-                    </tr>
-                    <tr>
-                        <td>Email</td>
+                        <td>&#128231 Email:</td>
                         <td><a href="mailto:220160340@aston.ac.uk">220160340@aston.ac.uk</a></td>
                     </tr>
                 </table>
