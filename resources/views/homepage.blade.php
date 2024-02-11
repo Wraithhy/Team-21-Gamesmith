9d9458532f9ebd9cdf6286dc164aaaf56b45fa87
Maluk1 <147244511+Maluk1@users.noreply.github.com>
2024-02-11 18:17:51 +0000

    minor changes to the homepage blade
    - wanted to keep the consistency of the coding by using the @extends and section

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,5 +1,4 @@
-@include('auth.layouts')
-<!DOCTYPE html>
+<!--<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -10,9 +9,15 @@
 
   </style>
   <link rel="stylesheet" href="{{ asset('css/homepage_style.css') }}">
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bebas+Neue">
-</head>
+</head>-->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bebas+Neue">
+
+@extends('auth.layouts')
+
+
+@section('content')
 <body>
+  <link rel="stylesheet" href="css/homepage_style.css">
 
 <div class="container" >
     <div class="text-center">
@@ -121,7 +126,11 @@
 </div>
 </div>
 
+
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
   </body>
 </body>
 </html>
+
+@endsection
