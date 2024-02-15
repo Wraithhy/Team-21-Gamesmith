f8e6abf7678b161951f76322ff57bf8483305366
Nayim159 <220226495@aston.ac.uk>
2024-02-15 14:50:25 +0000

    New updated format for contactus page
    Updated the format for the contact us page to look more formal, as well as updating the css to make the page more appealing and formal.

--- resources/views/contactus.blade.php
+++ resources/views/contactus.blade.php
@@ -1,79 +1,80 @@
-@include ('navbar')
+@extends('master')    
 
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset = "utf-8" />
-        <title>Contact Us</title>
-        <meta name="viewport" content="width=device-width inital-scale-1.0" />
-        <link rel="stylesheet" href="css/contact.css" />
-        <link href="https://fonts.googleapis.com/css?family=Bebas+Neue" rel="stylesheet">
+    <link rel="stylesheet" href="css/contact.css">
 
-        
-        
-    </head>
-    
-    <body>
-            <h1>Contact Us</h1>
-        <div class="container">
+@section( 'content' )
+
+<body>
+
+
+    <div class="container">
+        <h1> Connect with us</h1>
+        <p>We would love to get to know what you require from us!</p>
+        <div class="contact-us"> 
+            <div class="contact-left">
+                <h3>Send your request</h3>
+                <form id="myForm" onsubmit="compareEmails(this);">
+                    <div class="input-row">
+                        <div class="contact-info">
+                            <label for="name">Name</label>
+                            <input id="name" type="text" placeholder="John Smith">
+                        </div>
+                        <div class="contact-info">
+                            <label for="Phone">Phone</label>
+                            <input id="Phone" type="text" placeholder="+44 123 456 789 ">
+                        </div>
+                    </div>
+                    <div class="input-row">
+                        <div class="contact-info">
+                            <label for="email">Email</label>
+                            <input id="email" name="email" type="email" placeholder="johnsmith@gmail.com">
+                        </div>
+                        <div class="contact-info">                            
+                            <label for="Confirm-email">Confirm Email</label>
+                            <input id="Confirm-email" name="cEmail" type="email" placeholder="johnsmith@gmail.com">
+                        </div>
+                    </div>
+                    <div class="input-row">
+                        <label>How would you like to be contacted?</label>
+                        <div class="contact-info">
+                            <input id="html" type="checkbox"  name="CotactPref">
+                            <label for="html">Email</label><br>
+
+                        </div>
+                    <div class="contact-info">
+                        <input id="css" type="checkbox"  name="ContactPref">
+                        <label for="css">Phone</label>
+                    </div>
+                </div>
+                <label for="message">Message</label>
+                <textarea id="message" rows="5" placeholder="Your message here"></textarea>
+
+                    <button id="Submit" type="submit">Send</button>
+                </form>
+            </div>
             
-            <form action="mailto:gamesmith-ecommerce@gmail.com" method="post" enctype="text/plain" onsubmit="return validateForm()">
-              <label for="first_name">First Name: </label>
-              <input type ="text" id="first_name" name="first_name" required><br><br>
-              <label for="last_name">Last Name: </label>
-              <input type ="text" id="last_name" name="last_name" required><br><br>
-              <label for="contact_number">Contact Number: </label>
-              <input type ="tel" id="contact_number" name="contact_number" required><br><br>
-              <label for="email">Email Address: </label>
-              <input type ="email" id="email" name="email" required><br><br>
-              <label for="confirm_email">Confirm Email Address: </label>
-              <input type ="email" id="confirm_email" name="confirm_email" required><br><br>
-              <label for="message">Message: </label>
-              <textarea id="message" name="message" rows="5" cols="40" required></textarea><br><br>
+            <div class="contact-right">
+                <h3>Reach us </h3>
 
-              <input type="submit" value="Submit">
+                <table>
+                    <tr>
+                        <td>GameSmith</td>
+                        <td></td>
 
-            </form>
-          </div>
+                    </tr>
+                    <tr>
+                        <td>Group 21</td>
+                    </tr>
+                    <tr>
+                        <td>Email</td>
+                        <td><a href="mailto:220226495@aston.ac.uk">220226495@aston.ac.uk</a></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
 
-          
-
-        <footer>
-            @include('footer')
-        </footer>
-
-    </body>
+</body>
+@endSection
 
 
-    <script>
-      document.addEventListener('DOMContentLoaded', function() {
-        const form = document.querySelector('form');
-
-        form.addEventListener('submit', function(event) {
-        event.preventDefault();
-
-        const email = form.querySelector('#email').value.trim();
-        const confirmEmail = form.querySelector('#confirm_email').value.trim();
-
-        if (email !== confirmEmail) {
-            alert("Your email addresses don't match. Please double check.");
-        } else {
-            form.submit();
-        }
-
-        })
-      })
-
-      function validateForm() {
-        var project = document.forms[0]["first_name"].value;
-        var date = document.forms[0]["last_name"].value;
-        var duration = document.forms[0]["contact_number"].value;
-        var email = document.forms[0]["email"].value;
-        var confirmEmail = document.forms[0]["confirm_email"].value;
-        var contactMethod = document.forms[0]["message"].value;
-      }
-
-      
-
-    </script>
-</html>
