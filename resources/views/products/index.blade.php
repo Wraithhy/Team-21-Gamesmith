2abff7c9d920fb65cf408e7449ef8d5a1c43ea7f
MTQ2004 <146850945+MTQ2004@users.noreply.github.com>
2024-02-23 12:01:35 +0000

    CSS Styling to admin Index
    Styled it

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,26 +1,77 @@
 @include('layouts.app')
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Product</title>
+    <link href="https://fonts.googleapis.com/css?family=Bebas+Neue" rel="stylesheet">
+    <style>
+        body {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+            margin: 0;
+            background-color: #4A5859;
+        }
+
+        h1 {
+            font-size: 40px;
+            text-align: center;
+            color: #98D831;
+            margin: 20px 0;
+        }
+
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-top: 20px;
+        }
+
+        th,
+        td {
+            border: 1px solid #F0F2EF;
+            padding: 8px;
+            text-align: left;
+            font-family: 'Bebas Neue', sans-serif;
+        }
+
+        th {
+            background-color: #98D831;
+            color: #4A5859;
+        }
+
+        a {
+            display: block;
+            margin: 0 auto;
+            text-align: center;
+            color: #98D831;
+            text-decoration: underline;
+            cursor: pointer;
+            font-size: 18px; 
+        }
+
+        form {
+            display: inline-block;
+        }
+    </style>
 </head>
+
 <body>
-    <h1>Product</h1>
     <div>
         @if(session()->has('success'))
             <div>
-                {{session('success')}}
+                {{ session('success') }}
             </div>
         @endif
-    </div>
-    <div>
+
         <div>
-            <a href="{{route('product.create')}}">Create a Product</a>
+            <a href="{{ route('product.create') }}" style="font-family: 'Bebas Neue', sans-serif; font-size: 22px;">Create a Product</a>
         </div>
-        <table border="1">
+
+        <table>
             <tr>
                 <th>ID</th>
                 <th>Name</th>
@@ -32,16 +83,16 @@
             </tr>
             @foreach($products as $product )
                 <tr>
-                    <td>{{$product->id}}</td>
-                    <td>{{$product->name}}</td>
-                    <td>{{$product->qty}}</td>
-                    <td>{{$product->price}}</td>
-                    <td>{{$product->description}}</td>
+                    <td>{{ $product->id }}</td>
+                    <td>{{ $product->name }}</td>
+                    <td>{{ $product->qty }}</td>
+                    <td>{{ $product->price }}</td>
+                    <td>{{ $product->description }}</td>
                     <td>
-                        <a href="{{route('product.edit', ['product' => $product])}}">Edit</a>
+                        <a href="{{ route('product.edit', ['product' => $product]) }}">Edit</a>
                     </td>
                     <td>
-                        <form method="post" action="{{route('product.destroy', ['product' => $product])}}">
+                        <form method="post" action="{{ route('product.destroy', ['product' => $product]) }}">
                             @csrf 
                             @method('delete')
                             <input type="submit" value="Delete" />
@@ -52,4 +103,5 @@
         </table>
     </div>
 </body>
+
 </html>
