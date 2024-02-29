20b56981579d28e92ee4ac514c3636c92cec74cd
MTQ2004 <146850945+MTQ2004@users.noreply.github.com>
2024-02-29 15:17:04 +0000

    CSS for admin

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,47 +1,48 @@
-@include('layouts.app')
+@extends('layouts.app')
+@section('content')
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Create a Product</title>
+    <link href="https://fonts.googleapis.com/css?family=Bebas+Neue" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('/css/create.css') }}">
+    
 </head>
 <body>
     <h1>Create a Product</h1>
-    <div>
-        @if($errors->any())
+    @if($errors->any())
         <ul>
             @foreach($errors->all() as $error)
                 <li>{{$error}}</li>
             @endforeach
         </ul>
-
-
-        @endif
-    </div>
-    <form method="post" action="{{route('product.store')}}">
+    @endif
+    <form class="cprod" method="post" action="{{route('product.store')}}">
         @csrf 
         @method('post')
         <div>
             <label>Name</label>
-            <input type="text" name="name" placeholder="Name" />
+            <input class="name-input" type="text" name="name" placeholder="Name" />
         </div>
         <div>
             <label>Qty</label>
-            <input type="text" name="qty" placeholder="Qty" />
+            <input class="qty-input" type="text" name="qty" placeholder="Qty" />
         </div>
         <div>
             <label>Price</label>
-            <input type="text" name="price" placeholder="Price" />
+            <input class="price-input" type="text" name="price" placeholder="Price" />
         </div>
         <div>
             <label>Description</label>
-            <input type="text" name="description" placeholder="Description" />
+            <input class="description-input" type="text" name="description" placeholder="Description" />
         </div>
         <div>
-            <input type="submit" value="Save a New Product" />
+            <input class="submit-input1" type="submit" value="Save a New Product" />
         </div>
     </form>
 </body>
 </html>
+@endsection
--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -1,46 +1,45 @@
 @include('layouts.app')
-<!DOCTYPE html>
-<html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Edit a Product</title>
+    <link href="https://fonts.googleapis.com/css?family=Bebas+Neue" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('/css/edit.css') }}">
+
 </head>
 <body>
+
     <h1>Edit a Product</h1>
-    <div>
-        @if($errors->any())
+    @if($errors->any())
         <ul>
             @foreach($errors->all() as $error)
                 <li>{{$error}}</li>
             @endforeach
         </ul>
-
-
-        @endif
-    </div>
-    <form method="post" action="{{route('product.update', ['product' => $product])}}">
+    @endif
+    <form class="editProductForm" method="post" action="{{route('product.update', ['product' => $product])}}">
         @csrf 
         @method('put')
         <div>
             <label>Name</label>
-            <input type="text" name="name" placeholder="Name" value="{{$product->name}}" />
+            <input class="edit-n-input" type="text" name="name" placeholder="Name" value="{{$product->name}}" />
         </div>
         <div>
             <label>Qty</label>
-            <input type="text" name="qty" placeholder="Qty" value="{{$product->qty}}" />
+            <input class="edit-q-input" type="text" name="qty" placeholder="Qty" value="{{$product->qty}}" />
         </div>
         <div>
             <label>Price</label>
-            <input type="text" name="price" placeholder="Price" value="{{$product->price}}" />
+            <input class="edit-p-input" type="text" name="price" placeholder="Price" value="{{$product->price}}" />
         </div>
         <div>
             <label>Description</label>
-            <input type="text" name="description" placeholder="Description" value="{{$product->description}}" />
+            <input class="edit-desc-input" type="text" name="description" placeholder="Description" value="{{$product->description}}" />
         </div>
         <div>
-            <input type="submit" value="Update" />
+            <input class="edit-sbmt-input" type="submit" value="Update" />
         </div>
     </form>
 </body>
