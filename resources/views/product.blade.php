e394102eab62d89d1d958b9da04ca2328e4f0bd9
Khalil Rizwan <220231190@aston.ac.uk>
2024-03-14 16:35:43 +0000

    Repushing Final Homepage Changes

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -3,27 +3,27 @@
 <div class="custom-product">
     <div class="trending-wrapper">
         <div class="card">
-            <h3>Monitors</h3>
+        <a id="#MonitorJump"><h3>Monitors</h3></a>
             <img src="images/1.png" class="img-top">
         </div>
 
         <div class="card">
-            <h3>Headset</h3>
+        <a id="#HeadsetJump"><h3>Headset</h3>
             <img src="images/6.png" class="img-top">
         </div>
 
         <div class="card">
             <h3>Keyboards</h3>
-            <img src="images/11.png" class="img-top">
+            <a id="#KeyboardsJump"><img src="images/11.png" class="img-top">
         </div>
 
         <div class="card">
-            <h3>Mice</h3>
+        <a id="#MiceJump"> <h3>Mice</h3>
             <img src="images/16.png" class="img-top">
         </div>
 
         <div class="card">
-            <h3>Controllers</h3>
+        <a id="#ControllersJump"> <h3>Controllers</h3>
             <img src="images/21.png" class="img-top">
         </div>
     </div>
